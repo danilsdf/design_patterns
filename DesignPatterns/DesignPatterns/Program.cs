f02247f57252d7910a4cff788b5eda4a24fd5c13
Danil Kravchenko <danil.k@qualer.com>
2022-11-15 00:03:10 +0200

    init

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using AbstractFactoryPattern;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main()
+        {
+            RunAbstractFactory();
+        }
+
+        static void RunAbstractFactory()
+        {
+            var t = new AbstractFactoryMain();
+
+            t.Run();
+        }
+    }
+}
